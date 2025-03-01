27a3c5942906629ae6156e66237ac10486263acc
manu <man.koell@gmail.com>
2025-03-02 00:27:44 +0100

    clean up and update for emacs 30

---
+++ custom.el
@@ -0,0 +1,20 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(package-selected-packages
+   '(consult corfu evil-commentary evil-escape evil-surround gptel hungry-delete
+			 magit marginalia ol-notmuch orderless org-download spacious-padding
+			 try vertico))
+ '(tab-bar-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Iosevka Fixed" :foundry "UKWN" :slant normal :weight regular :height 160 :width normal)))))
--- init.el
+++ init.el
@@ -1,11 +1,14 @@
-;;; use-package
+;;; package.el
 (require 'package)
+
 (setq package-enable-at-startup nil)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")))
+(setq package-archives
+      '(("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ("elpa" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
+;;; use-package
 (use-package use-package
   :custom
   (use-package-always-ensure t)
@@ -14,34 +17,32 @@
 ;;; emacs
 (use-package emacs
   :custom
+  (inhibit-startup-screen t)
+  (initial-buffer-choice "~/org/fhv.org")
+
   (fill-column 80)
   (tab-width 4)
-  (default-directory "~/fhv/ws24/")
   (frame-resize-pixelwise t)
   (delete-by-moving-to-trash t)
-  (inhibit-startup-screen t)
-  (select-enable-clipboard t)
   (help-window-select t)
-  (mac-command-modifier 'meta)
-  (mac-option-modifier nil)
   (use-dialog-box nil)
-  (backup-directory-alist '(("." . "~/.emacs.d/backup")))
+  (vc-follow-symlinks t)
+
+  ;; autosave and backup
   (version-control t)
   (delete-old-versions -1)
   (auto-save-list-file-prefix "~/.emacs.d/autosave/")
   (auto-save-file-name-transforms '((".*" "~/.emacs.d/autosave/" t)))
   (lock-file-name-transforms '((".*" "~/.emacs.d/lock/" t)))
-  (bookmark-default-file "~/.emacs.d/bookmarks")
-  (global-auto-revert-non-file-buffers t)
+  (backup-directory-alist '(("." . "~/.emacs.d/backup")))
+
+  ;; completion
   (read-file-name-completion-ignore-case t)
   (read-buffer-completion-ignore-case t)
   (completion-ignore-case t)
-  (vc-follow-symlinks t)
-  (initial-buffer-choice "~/org/fhv.org")
-  (modus-themes-mode-line '(accented borderless))
-
   (tab-always-indent 'complete)
 
+  ;; ibuffer
   (ibuffer-show-empty-filter-groups nil)
   (ibuffer-display-summary nil)
   (ibuffer-formats
@@ -55,7 +56,6 @@
                                                   (name . "^\\*.*\\*$")
                                                   (name . "^\\*scratch\\*$")
                                                   (name . "^\\*Messages\\*$")))))))
-
   :config
   (blink-cursor-mode 0)
   (scroll-bar-mode 0)
@@ -66,13 +66,13 @@
   (show-paren-mode)
   (global-auto-revert-mode)
   (global-hl-line-mode)
-  (defalias 'yes-or-no-p 'y-or-n-p)
 
+  (defalias 'yes-or-no-p 'y-or-n-p)
   (advice-add #'indent-for-tab-command :after #'hippie-expand)
 
   (add-hook 'after-make-frame-functions
             (lambda (frame)
-              (let ((font-height (if (string= system-name "M720q") 160 130)))
+              (let ((font-height (if (string= system-name "M720t") 160 130)))
                 (with-selected-frame frame
                   (set-face-attribute 'default nil :font "Iosevka" :height font-height)))))
 
@@ -84,8 +84,7 @@
 				(evil-local-set-key 'normal (kbd "TAB") 'outline-toggle-children)
 				(outline-hide-sublevels 1))))
 
-  (load-theme 'modus-vivendi)
-  (modus-themes-load-vivendi)
+  (load-theme 'modus-vivendi-deuteranopia)
 
   :bind*
   ("C-x C-b" . ibuffer)
@@ -104,6 +103,7 @@
   ;; (jupyter-python-mode . python-mode)
   (ibuffer-mode . (lambda () (ibuffer-switch-to-saved-filter-groups "default"))))
 
+;;; tabs
 (use-package tab-bar
   :after org
   :custom
@@ -121,6 +121,7 @@
   ("M-h" . tab-previous)
   ("M-l" . tab-next))
 
+;;; dired
 (use-package dired
   :ensure nil
   :custom
@@ -129,63 +130,22 @@
   (dired-auto-revert-buffer t)
   (dired-listing-switches "-lh --group-directories-first"))
 
-
-
 ;;; orgmode
 (use-package org
   :custom
-  (org-attach-method "lns")
   (org-startup-indented t)
-  (org-startup-with-beamer-mode t)
-  (org-directory "~/fhv/ws24/")
-  (org-default-notes-file "~/org/notes.org")
-  (org-archive-location "~/org/archive.org::* ARCHIVE")
   (org-agenda-window-setup 'only-window)
+  (org-src-window-setup 'current-window)
+
+  (org-archive-location "~/org/archive.org::* ARCHIVE")
   (org-agenda-files (append (directory-files-recursively "~/org" "\.org$")))
   (org-refile-targets '((org-agenda-files . (:maxlevel . 3))))
   (org-refile-use-outline-path 'file)
   (org-refile-allow-creating-parent-nodes 'confirm)
   (org-outline-path-complete-in-steps nil)
-  (org-src-window-setup 'current-window)
+
   (org-confirm-babel-evaluate nil)
   (org-id-link-to-org-use-id t)
-  (org-M-RET-may-split-line '((default . nil)))
-  ;; (org-babel-jupyter-override-src-block "python")
-  (org-html-doctype "html5")
-  (org-html-html5-fancy t)
-  (org-publish-project-alist
-   '(("blog"
-	  :base-directory "~/blog"
-	  :publishing-function org-html-publish-to-html
-	  :publishing-directory "~/blog"
-	  :section-numbers nil
-	  :with-toc nil
-	  :with-author nil
-	  :with-creator t
-	  :with-date nil
-	  :time-stamp-file nil
-	  :html-self-link-headlines t
-	  :html-head "<link rel=\"stylesheet\" href=\"style.css\">"
-	  ;; :html-link-home "./"
-	  ;; :html-home/up-format "<nav><a href=\"%s\">fifteen eleven ninety</a></nav>"
-	  ;; :html-postamble nil
-	  ;; :html-preamble nil
-	  :html-head-include-scripts nil
-	  :html-head-include-default-style nil)))
-  (org-agenda-custom-commands
-   '(("f" "Filtered Agenda"
-      agenda "" 
-      ((org-agenda-skip-function
-        (lambda ()
-          (let ((heading (org-get-heading t t t t)))
-            (if (or (string-match-p "software engineering" (downcase heading))
-                    (string-match-p "web applications" (downcase heading))
-                    (string-match-p "agile" (downcase heading))
-                    (string-match-p "technical writing" (downcase heading)))
-                (point)  ;; Skip this entry by returning the point
-              nil))))))))
-  (org-export-with-smart-quotes t)
-  (org-html-validation-link nil)
   (org-latex-hyperref-template "\\usepackage[colorlinks=false]{hyperref}")
   (org-latex-pdf-process
    '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
@@ -199,8 +159,6 @@
                                (java . t)
                                (shell . t)
                                (sql . t)
-                               ;; (jupyter . t)
-                               ;; (plantuml .t)
                                (sqlite . t)))
 
   :hook
@@ -208,8 +166,6 @@
   (org-babel-after-execute . org-redisplay-inline-images))
 
 (use-package org-download)
-
-
 
 ;;; evil
 (use-package evil
@@ -280,7 +236,6 @@
 
 ;;; git
 (use-package magit)
-
 ;;; misc
 (use-package try)
 (use-package which-key
@@ -298,46 +253,12 @@
   (gptel-default-mode 'org-mode)
   (gptel-prompt-prefix-alist '((org-mode . "")))
   (gptel-response-prefix-alist '((org-mode . "")))
+
   :hook
   (org-mode . (lambda ()
 				(when (equal (buffer-file-name) (expand-file-name "~/org/gptel.org"))
 				  (gptel-mode 1)))))
-  ;; (gptel-post-response . (lambda (beg end)
-  ;; 						   (save-excursion
-  ;; 							 (goto-char beg)
-  ;; 							 (push-mark)
-  ;; 							 (goto-char end)
-  ;; 							 (activate-mark)
-  ;; 							 (org-fill-paragraph nil 'region)))))
-
-;;; java
-(use-package eglot-java
-  :hook
-  (java-mode . eglot-java-mode)
-  (before-save . (lambda ()
-                   (when (bound-and-true-p eglot--managed-mode)
-                     (eglot-format-buffer))))
-  :config
-  (remove-hook 'eldoc-display-functions 'eldoc-display-in-echo-area))
-
-(use-package indent-bars
-  :hook (java-mode . indent-bars-mode))
-
-;;; apps
-(use-package elfeed
-  :custom
-  (elfeed-search-filter "")
-  (elfeed-feeds
-   '(("https://rss.orf.at/news.xml" news)
-	 ("https://hnrss.org/frontpage?link=comments")
-	 ("https://workingdraft.de/feed/" podcast web)
-	 ("https://www.baeldung.com/feed" java)
-	 ("https://hnrss.org/newest?q=java&link=comments" java)
-	 ("https://hnrss.org/newest?q=emacs&link=comments" emacs)
-	 ("https://old.reddit.com/r/emacs/.rss" emacs)
-	 ("https://hnrss.org/newest?q=vim&link=comments" vim)
-	 )))
-
 ;;; custom
-(custom-file (concat user-emacs-directory "custom.el"))
-(load-file custom-file)
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load-file custom-file))
