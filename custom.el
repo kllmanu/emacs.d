6ca9458e51080bea60fa012cca9fec2591b3180d
manu <man.koell@gmail.com>
2025-04-01 10:54:01 +0200

    fix end of file

--- custom.el
+++ custom.el
@@ -15,8 +15,3 @@
  '(package-selected-packages nil)
  '(tab-bar-mode t)
  '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.)
